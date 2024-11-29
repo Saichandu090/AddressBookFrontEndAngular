ef9054455b8ac5919510083cdc7d71235811503a
Maddineni Pujitha <marrisaichandu143@gmail.com>
2024-11-29 16:23:15 +0530

    Updated Buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AddFormComponent } from './add-form/add-form.component';
-import { NgModel } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 
 export const routes: Routes = [
